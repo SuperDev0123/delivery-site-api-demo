23de9478a2043c3807ae14804fe995e2abf46a4d
Anil Kumar <anil.stunning09@gmail.com>
2018-11-02 11:50:57 +0530

    updated

--- DeliverMe/urls.py
+++ DeliverMe/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls')), 
     path('', include('Pages.urls')),
-    path('', include('Accounts.urls'))
+   # path('', include('Accounts.urls'))
 ]
