5a2fd486d4cb999fa4d53510ed91fea85f4b13b1
Anil Kumar <anil.stunning09@gmail.com>
2018-11-03 13:24:07 +0530

    put upload file code

--- Pages/views.py
+++ Pages/views.py
@@ -1,4 +1,10 @@
 from django.views.generic.base import TemplateView
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
 
 
 class HomePageView(TemplateView):
@@ -7,4 +13,14 @@ class HomePageView(TemplateView):
 
 
 class SharePageView(TemplateView):
-    template_name = 'pages/share.html'    
+    template_name = 'pages/share.html'   
+
+def handle_uploaded_file(f):
+    with open('media/onedrive/' + f.name, 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+def upload(request):
+	handle_uploaded_file(request.FILES['file'])
+	html = "<html><body>Uploaded File :  %s </body></html>"  % request.FILES['file'].name      
+	return HttpResponse(html)
