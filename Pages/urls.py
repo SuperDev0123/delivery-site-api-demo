3336d3fd0ab4d53170185f6b83c3410434a7fd0b
Anil Kumar <anil.stunning09@gmail.com>
2018-11-03 13:23:38 +0530

    updated

--- Pages/urls.py
+++ Pages/urls.py
@@ -1,9 +1,10 @@
 # pages/urls.py
 from django.urls import path
 
-from .views import HomePageView, SharePageView # new
-
+from .views import HomePageView, SharePageView  # new
+from . import views
 urlpatterns = [
     path('share/', SharePageView.as_view(), name='share'), # new
+    path('share/upload/', views.upload, name='upload'), # new
     path('', HomePageView.as_view(), name='home'),  
 ]
