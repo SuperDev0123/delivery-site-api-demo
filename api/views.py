5d797cc9b0bab41d028e40ac84f44a1deacbee4f
smartworld <smartworld@email.com>
2019-01-14 22:26:09 +0100

    fix: redis key issue

--- api/views.py
+++ api/views.py
@@ -139,11 +139,11 @@ class FileUploadView(views.APIView):
         warehouse_id = request.POST.get('warehouse_id')
         clientWarehouseObject = Client_warehouses.objects.filter(pk_id_client_warehouses__contains=warehouse_id)
         upload_file_name = request.FILES['file'].name
-        prepend_name = str(dme_account_num) + '_' + upload_file_name
+        prepend_name = upload_file_name
 
-        save2Redis(prepend_name + "l_000_client_acct_number", dme_account_num)
-        save2Redis(prepend_name + "l_011_client_warehouse_id", warehouse_id)
-        save2Redis(prepend_name + "l_012_client_warehouse_name", clientWarehouseObject[0].warehousename)
+        save2Redis(upload_file_name + "l_000_client_acct_number", dme_account_num)
+        save2Redis(upload_file_name + "l_011_client_warehouse_id", warehouse_id)
+        save2Redis(upload_file_name + "l_012_client_warehouse_name", clientWarehouseObject[0].warehousename)
 
         handle_uploaded_file(request, dme_account_num, request.FILES['file'])
 
