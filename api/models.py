795b94563040f4c91dca0120eb2baf647cf8a3d1
smartworld <smartworld@email.com>
2019-01-21 19:39:50 +0100

    add: new field to booking and bok_1: b_client_warehouse_code

--- api/models.py
+++ api/models.py
@@ -254,6 +254,7 @@ class Bookings(models.Model):
 	b_client_sales_inv_num = models.CharField(verbose_name=_('Sales Inv Num'), max_length=64, blank=True, null=True, default='')
 	b_client_order_num = models.CharField(verbose_name=_('Order Num'), max_length=64, blank=True, null=True, default='')
 	b_client_del_note_num = models.CharField(verbose_name=_('Del Note Num'), max_length=64, blank=True, null=True, default='')
+	b_client_warehouse_code = models.CharField(verbose_name=_('Warehouse code'), max_length=64, blank=True, null=True, default='')
 
 	class Meta:
 		db_table = 'dme_bookings'
@@ -420,6 +421,7 @@ class BOK_1_headers(models.Model):
 	b_client_order_num = models.CharField(verbose_name=_('Order Num'), max_length=64, blank=True, null=True, default='')
 	b_client_del_note_num = models.CharField(verbose_name=_('Del Note Num'), max_length=64, blank=True, null=True, default='')
 	z_createdTimeStamp = models.DateTimeField(verbose_name=_('z_createdTimeStamp'), default=timezone.now, null=True)
+	b_client_warehouse_code = models.CharField(verbose_name=_('Warehouse code'), max_length=64, blank=True, null=True, default='')
 
 	class Meta:
 		db_table = 'bok_1_headers'
