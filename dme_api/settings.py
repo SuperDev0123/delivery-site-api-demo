83a94e245c5e888419559103f7086d8bfe1ce42c
smartworld <smartworld@email.com>
2019-05-07 03:52:18 +0300

    mod: smtp config

--- dme_api/settings.py
+++ dme_api/settings.py
@@ -190,7 +190,7 @@ ENV = 'local' # local, dev, prod
 # Email setting
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.office365.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'your@gmail.com'
-EMAIL_HOST_PASSWORD = 'pwd'
+EMAIL_HOST_USER = '***'
+EMAIL_HOST_PASSWORD = '***'
