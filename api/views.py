39c8bdd08d3f8fbae07d459cc81039f9b08bf2f4
smartworld <smartworld@email.com>
2019-05-09 00:51:21 +0300

    mod: uncomment

--- api/views.py
+++ api/views.py
@@ -1505,10 +1505,10 @@ def download_csv(request):
 
     for booking_id in booking_ids:
         booking = Bookings.objects.get(id=booking_id)
-        # booking.b_dateBookedDate = datetime.now()
-        # booking.b_status = 'Booked CSV'
-        # booking.v_FPBookingNumber = 'DME' + str(booking.b_bookingID_Visual)
-        # booking.save()
+        booking.b_dateBookedDate = datetime.now()
+        booking.b_status = 'Booked CSV'
+        booking.v_FPBookingNumber = 'DME' + str(booking.b_bookingID_Visual)
+        booking.save()
 
         booking_lines = Booking_lines.objects.filter(fk_booking_id=booking.pk_booking_id)
         index = 1
