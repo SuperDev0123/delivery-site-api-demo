64f4d7715fa9b88ce030f4c8deb42467d3d3f82c
smartworld <smartworld@email.com>
2019-07-25 22:33:09 +0200

    add: missing lib for barcode creator

--- api/utils.py
+++ api/utils.py
@@ -37,18 +37,11 @@ from reportlab.platypus import (
 from reportlab.platypus.flowables import Spacer, HRFlowable, PageBreak, Flowable
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.units import inch, mm
-from reportlab.graphics.barcode import (
-    code39,
-    code128,
-    code93,
-    createBarcodeDrawing,
-    eanbc,
-    qr,
-    usps,
-    renderPDF,
-)
+from reportlab.graphics.barcode import code39, code128, code93, createBarcodeDrawing, eanbc, qr, usps
+from reportlab.graphics.barcode import 
 from reportlab.graphics.shapes import Drawing
 from reportlab.pdfgen import canvas
+from reportlab.graphics import renderPDF
 from reportlab.lib import colors
 
 from django.core.mail import send_mail
