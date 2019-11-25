134c5b6a74918579ba8cabde1ce8fa8f57a4f303
smartworld <smartworld@email.com>
2019-11-25 04:12:20 +0100

    mod: take out ' Allied'

--- api/fp_apis/apis.py
+++ api/fp_apis/apis.py
@@ -877,7 +877,7 @@ def pricing(request):
                 _set_error(booking, error_msg)
                 return JsonResponse({"message": error_msg}, status=400)
 
-            fp_names = ["Sendle", "Capital", "Hunter", "TNT", "Allied", "Fastway"]
+            fp_names = ["Sendle", "Capital", "Hunter", "TNT", "Fastway"]  # "Allied"
 
             try:
                 for fp_name in fp_names:
