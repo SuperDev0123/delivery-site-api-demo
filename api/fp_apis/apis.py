4c20539fb66f8af046f3775da49b8a572a3c6a68
smartworld-dm <smartworld@email.com>
2020-04-05 16:19:30 +0200

    fix: Calc ETA dates after REBOOK

--- api/fp_apis/apis.py
+++ api/fp_apis/apis.py
@@ -1254,7 +1254,9 @@ def rebook(request, fp_name):
                     booking.fk_fp_pickup_id = json_data["consignmentNumber"]
                     booking.b_dateBookedDate = str(datetime.now())
                     booking.b_status = "PU Rebooked"
-                    booking.b_error_Capture = ""
+                    booking.s_05_Latest_Pick_Up_Date_TimeSet = booking.get_eta_pu_by()
+                    booking.s_06_Latest_Delivery_Date_TimeSet = booking.get_eta_de_by()
+                    booking.b_error_Capture = None
                     booking.save()
 
                     status_history.create(
