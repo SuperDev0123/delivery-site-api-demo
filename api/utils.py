0644319d9a4aa6acb84320d1729d1df7be6b4c3b
smartworld-dm <smartworld@email.com>
2020-04-14 08:36:15 +0200

    fix: send_email minor issue

--- api/utils.py
+++ api/utils.py
@@ -23,6 +23,7 @@ from os.path import basename
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 from email.utils import COMMASPACE, formatdate
 
 from reportlab.lib.enums import TA_JUSTIFY, TA_RIGHT, TA_CENTER, TA_LEFT
