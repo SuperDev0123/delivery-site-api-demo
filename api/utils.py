20943abefc2c33981585c1243e3d039ce9e63ecf
smartworld-dm <smartworld@email.com>
2020-04-21 04:16:29 +0200

    fix: get_pu_by

--- api/utils.py
+++ api/utils.py
@@ -5295,8 +5295,16 @@ def get_pu_by(booking):
         pu_by = datetime.combine(
             booking.pu_PickUp_By_Date,
             time(
-                int(booking.pu_PickUp_By_Time_Hours),
-                int(booking.pu_PickUp_By_Time_Minutes),
+                int(
+                    booking.pu_PickUp_By_Time_Hours
+                    if booking.pu_PickUp_By_Time_Hours
+                    else 0
+                ),
+                int(
+                    booking.pu_PickUp_By_Time_Minutes
+                    if booking.pu_PickUp_By_Time_Minutes
+                    else 0
+                ),
                 0,
             ),
         )
@@ -5365,26 +5373,28 @@ def get_eta_de_by(booking, quote):
         logger.error(f"Error #1002: {e}")
         return None
 
+
 def get_b_bookingID_Visual(dme_file):
-    b_bookingID_Visual = ''
-    if dme_file.file_type == 'xls import' and (
+    b_bookingID_Visual = ""
+    if dme_file.file_type == "xls import" and (
         dme_file.note and len(dme_file.note) == 36
     ):
-        booking = Bookings.objects.filter(pk_booking_id = dme_file.note).first()
+        booking = Bookings.objects.filter(pk_booking_id=dme_file.note).first()
 
         if booking:
             b_bookingID_Visual = booking.b_bookingID_Visual
-        
+
     return b_bookingID_Visual
 
-def get_booking_id(dme_file): 
-    booking_id = ''
-    if dme_file.file_type == 'xls import' and (
+
+def get_booking_id(dme_file):
+    booking_id = ""
+    if dme_file.file_type == "xls import" and (
         dme_file.note and len(dme_file.note) == 36
     ):
-        booking = Bookings.objects.filter(pk_booking_id = dme_file.note).first()
+        booking = Bookings.objects.filter(pk_booking_id=dme_file.note).first()
 
         if booking:
             booking_id = booking.id
-        
+
     return booking_id
