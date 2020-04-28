31ca5b6f27cb83ae7e2cfa9801647bcfe2669fd4
smartworld-dm <smartworld@email.com>
2020-04-28 05:38:18 +0200

    fix: Get actual_delivery_timestamp

--- api/fp_apis/operations/tracking.py
+++ api/fp_apis/operations/tracking.py
@@ -70,7 +70,7 @@ def update_booking_with_tracking_result(request, booking, fp_name, consignmentSt
 
         if result:
             booking.s_21_Actual_Delivery_TimeStamp = result
-            booking.delivery_booking = result
+            booking.delivery_booking = result[:10]
 
     # Update booking's latest status
     if fp_name.lower() == "startrack":
