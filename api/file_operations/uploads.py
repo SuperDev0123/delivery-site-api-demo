78b61c95e4e19fd60cf1966b7660e5565732cf6e
smartworld-dm <smartworld@email.com>
2020-05-08 04:45:17 +0200

    fix: upload attachment

--- api/file_operations/uploads.py
+++ api/file_operations/uploads.py
@@ -89,7 +89,7 @@ def upload_attachment_file(user_id, file, booking_id, upload_option):
             fp_dir_name = (
                 f"{fp.fp_company_name.lower()}_{fp.fp_address_country.lower()}"
             )
-            file_path = f"{settings.STATIC_PUBLIC}/attachments/{fp_dir_name}/"
+            file_path = f"{settings.STATIC_PUBLIC}/attachment/{fp_dir_name}/"
 
             if not os.path.isdir(file_path):
                 os.makedirs(file_path)
@@ -128,7 +128,7 @@ def upload_attachment_file(user_id, file, booking_id, upload_option):
             for chunk in file.chunks():
                 destination.write(chunk)
 
-        if upload_option == "attachments":
+        if upload_option == "attachment":
             dme_attachment = Dme_attachments(
                 fk_id_dme_client=client,
                 fk_id_dme_booking=booking.pk_booking_id,
