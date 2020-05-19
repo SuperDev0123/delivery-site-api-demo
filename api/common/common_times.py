8526f53a7a93cef54a901d9e50e90cef1d32c0b7
smartworld-dm <smartworld@email.com>
2020-05-19 04:32:02 +0200

    fix: timezone

--- api/common/common_times.py
+++ api/common/common_times.py
@@ -3,9 +3,10 @@ from datetime import timedelta, datetime
 
 
 def get_sydney_now_time(return_type="char"):
-    sydney_tz = pytz.timezone("Australia/Sydney")
-    sydney_now = sydney_tz.localize(datetime.utcnow())
-    sydney_now = sydney_now + timedelta(hours=10)
+    # sydney_tz = pytz.timezone("Australia/Sydney")
+    # sydney_now = sydney_tz.localize(datetime.utcnow())
+    # sydney_now = sydney_now + timedelta(hours=10)
+    sydney_now = datetime.now()
 
     if return_type == "char":
         return sydney_now.strftime("%Y-%m-%d %H:%M:%S")
