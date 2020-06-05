fb2fa40fcdf8eaa2e8d77b01ae11cb2996feb047
royalone <greatroyalone@outlook.com>
2020-06-05 22:53:59 +0800

    fix url base name for client products

--- api/urls.py
+++ api/urls.py
@@ -44,7 +44,7 @@ router.register(r"bok_3_lines_data", BOK_3_ViewSet, basename="bok3")
 router.register(r"files", FilesViewSet, basename="files")
 router.register(r"bookingsets", BookingSetsViewSet, basename="bookingsets")
 router.register(r"clientemployee", ClientEmployeesViewSet, basename="clientemployee")
-router.register(r"clientproducts", ClientProductsViewSet, basename="pricing_rules")
+router.register(r"clientproducts", ClientProductsViewSet, basename="client_products")
 urlpatterns = router.urls
 
 urlpatterns += [
