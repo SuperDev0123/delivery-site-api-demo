945303d1ffd147e52ad723e31f5395e522fb1607
smartworld-dm <smartworld@email.com>
2020-08-27 08:19:08 +0200

    add: a logger for PUSH api client name

--- api/views_api.py
+++ api/views_api.py
@@ -165,6 +165,7 @@ def boks(request):
         client_employee = Client_employees.objects.get(fk_id_user_id=request.user.pk)
         client = client_employee.fk_id_dme_client
         client_name = client.company_name
+        logger.info(f"@810 - client: , {client_name}")
     except Exception as e:
         logger.info(f"@811 - client_employee does not exist, {str(e)}")
         message = "You are not allowed to use this api-endpoint."
