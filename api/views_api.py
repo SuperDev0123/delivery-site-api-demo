47d35f855ae913ea64bcc02c7be680732a5e0d8d
smartworld-dm <smartworld@email.com>
2021-01-17 07:39:11 +0100

    mod: ETA readable on status page decimal points

--- api/views_api.py
+++ api/views_api.py
@@ -1910,7 +1910,7 @@ def get_delivery_status(request):
 
         if quote:
             quote_data = SimpleQuoteSerializer(quote).data
-            quote_data["eta_readable"] = get_etd_in_hour(quote) / 24
+            quote_data["eta_readable"] = round(get_etd_in_hour(quote) / 24, 2)
 
         if category == "Booked":
             step = 2
