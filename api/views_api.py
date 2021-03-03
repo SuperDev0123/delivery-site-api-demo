4c7281ecb627d92c5b4506b7b5d0133ff39cfc20
smartworld-dm <smartworld@email.com>
2021-03-03 03:04:01 +0100

    fix: avail from date from 2 days after

--- api/views_api.py
+++ api/views_api.py
@@ -1374,7 +1374,7 @@ def push_boks(request):
 
             if not bok_1.get("b_021_b_pu_avail_from_date"):
                 bok_1["b_021_b_pu_avail_from_date"] = str(
-                    datetime.now() + timedelta(days=3)
+                    datetime.now() + timedelta(days=2)
                 )[:10]
 
         # Populate default values
@@ -1789,7 +1789,7 @@ def partial_pricing(request):
 
     booking = {
         "pk_booking_id": bok_1["pk_header_id"],
-        "puPickUpAvailFrom_Date": str(datetime.now() + timedelta(days=3))[:10],
+        "puPickUpAvailFrom_Date": str(datetime.now() + timedelta(days=2))[:10],
         "b_clientReference_RA_Numbers": "initial_RA_num",
         "puCompany": warehouse.name,
         "pu_Contact_F_L_Name": "initial_PU_contact",
