cfc781766b276f9d908e5061c5834391e8293562
smartworld-dm <smartworld@email.com>
2021-03-16 03:54:31 +0100

    fix: ship-it label

--- api/operations/labels/ship_it.py
+++ api/operations/labels/ship_it.py
@@ -398,11 +398,17 @@ def build_label(booking, filepath, lines=[], label_index=0):
         tbl_data1 = [
             [
                 Paragraph(
-                    "<font size=%s><b>TO:</b> %s %s</font>"
+                    "<font size=%s><b>TO:</b> %s</font>"
+                    % (label_settings["font_size_medium"], booking.puCompany or ""),
+                    style_left,
+                )
+            ],
+            [
+                Paragraph(
+                    "<font size=%s>  %s</font>"
                     % (
                         label_settings["font_size_medium"],
                         booking.de_to_Contact_F_LName,
-                        (booking.puCompany) if (booking.puCompany) else "",
                     ),
                     style_left,
                 )
