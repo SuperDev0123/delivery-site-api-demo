22cbbd074a038bc771d209997716c3e740069b0b
smartworld-dm <smartworld@email.com>
2021-05-29 10:33:28 +0200

    feat(jason_l): populate DIMs by ProductGroupCode

--- api/operations/product_operations.py
+++ api/operations/product_operations.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 from rest_framework.exceptions import ValidationError
 
 from api.models import Client_Products
+from api.clients.jason_l.constants import DIM_BY_GROUP_CODE as JASONL_DIM_BY_GROUP_CODE
 
 logger = logging.getLogger(__name__)
 
@@ -101,6 +102,24 @@ def get_product_items(bok_2s, client, is_web=False):
                     "e_type_of_packaging": e_type_of_packaging or "Carton",
                 }
                 results = _append_line(results, line, qty)
+
+    # Jason L: populate DIMs by ProductGroupCode
+    for result in results:
+        if (
+            result["zbl_102_text_2"]
+            and result["e_dimLength"] == 1
+            and result["e_dimWidth"] == 1
+            and result["e_dimHeight"] == 1
+        ):
+            if result["zbl_102_text_2"] in JASONL_DIM_BY_GROUP_CODE:
+                logger.info(
+                    f"[GET PRODUCT ITEMS] Dims with GroupCode: {result['zbl_102_text_2']}, ItemCode: {result['e_item_type']}"
+                )
+                dims = JASONL_DIM_BY_GROUP_CODE[result["zbl_102_text_2"]]
+                result["e_dimLength"] = dims["length"]
+                result["e_dimWidth"] = dims["width"]
+                result["e_dimHeight"] = dims["height"]
+                result["e_weightPerEach"] = dims["weight"]
 
     logger.info(f"[GET PRODUCT ITEMS] {results}")
     return results
