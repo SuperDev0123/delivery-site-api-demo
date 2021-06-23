fd754fc8a8d10c9df6a6b862b2dd35775401fc41
smartworld-dm <smartworld@email.com>
2021-06-23 07:48:35 +0200

    fix: get_manifest for JasonL only for bookings not Booked

--- api/views.py
+++ api/views.py
@@ -3966,9 +3966,9 @@ def get_manifest(request):
     vx_freight_provider = body["vx_freight_provider"]
     username = body["username"]
 
-    bookings = Bookings.objects.filter(pk__in=booking_ids).only(
-        "id", "vx_freight_provider"
-    )
+    bookings = Bookings.objects.filter(
+        pk__in=booking_ids, b_dateBookedDate__isnull=True
+    ).only("id", "vx_freight_provider")
     fps = {}
 
     for booking in bookings:
