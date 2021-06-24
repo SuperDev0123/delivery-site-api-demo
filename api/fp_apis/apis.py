a343efc7dece3e1b1dde9c48b0908ef8555af99b
smartworld-dm <smartworld@email.com>
2021-06-24 06:47:59 +0200

    deactivate: Tracking log

--- api/fp_apis/apis.py
+++ api/fp_apis/apis.py
@@ -66,8 +66,8 @@ def tracking(request, fp_name):
 
         json_data = json.loads(res_content)
         s0 = json.dumps(json_data, indent=2, sort_keys=True)  # Just for visual
-        # enabled on 2021-06-24
-        logger.info(f"### Response ({fp_name} tracking): {s0}")
+        # disabled on 2021-06-24
+        # logger.info(f"### Response ({fp_name} tracking): {s0}")
 
         try:
             Log(
