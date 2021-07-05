53d10cb1251ed03b52d0a9dbdc029cdd25559184
smartworld-dm <smartworld@email.com>
2021-07-06 01:54:53 +0200

    fix: default address type

--- api/clients/jason_l/index.py
+++ api/clients/jason_l/index.py
@@ -223,7 +223,7 @@ def push_boks(payload, client, username, method):
         bok_1["shipping_type"] = bok_1.get("shipping_type", "DMEM").strip()
 
     bok_1["b_053_b_del_address_type"] = (
-        bok_1["b_053_b_del_delivery_type"].strip().lower()
+        bok_1.get("b_053_b_del_delivery_type", "business").strip().lower()
     )
     del bok_1["b_053_b_del_delivery_type"]
 
