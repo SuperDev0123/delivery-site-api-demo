6afad36a675ca7c16154180fad41dddfecba4df1
smartworld-dm <smartworld@email.com>
2021-07-26 04:59:20 +0200

    fix: picked status update

--- api/clients/jason_l/index.py
+++ api/clients/jason_l/index.py
@@ -1476,7 +1476,7 @@ def scanned(payload, client):
         f"http://{settings.WEB_SITE_IP}/label/{booking.b_client_booking_ref_num}/"
     )
     booking.z_downloaded_shipping_label_timestamp = datetime.utcnow()
-    booking.status = "picked"
+    status_history.create(booking, "Picked", "jason_l")
     booking.save()
 
     res_json = {"labelUrl": booking.z_label_url}
