dd5e8cd13aea8ea783c899eceee6d1468528a417
smartworld-dm <smartworld@email.com>
2021-08-10 06:49:38 +0200

    fix: Status page

--- api/views_client.py
+++ api/views_client.py
@@ -898,14 +898,20 @@ def get_delivery_status(request):
             elif index >= step:
                 timestamps.append("")
             else:
-                if category == "Complete" and index == 4:
+                if (
+                    category == "Complete"
+                    and not booking.b_status in ["Closed", "Cancelled"]
+                    and index == 4
+                ):
                     timestamps.append(
                         booking.s_21_Actual_Delivery_TimeStamp.strftime(
                             "%d/%m/%Y %H:%M"
                         )
                     )
                 else:
-                    status_time = get_status_time_from_category(booking.pk_booking_id, item)
+                    status_time = get_status_time_from_category(
+                        booking.pk_booking_id, item
+                    )
                     timestamps.append(
                         convert_to_AU_SYDNEY_tz(status_time).strftime("%d/%m/%Y %H:%M")
                         if status_time
