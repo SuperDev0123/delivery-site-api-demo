f975a36274a9569ae7a554495b4a6d004d8f56f4
smartworld-dm <smartworld@email.com>
2021-09-21 06:56:21 +0200

    fix: ptl & dtl

--- api/fp_apis/operations/surcharge/camerons.py
+++ api/fp_apis/operations/surcharge/camerons.py
@@ -1,20 +1,24 @@
 def ptl(param):
-    if int(param["pu_tail_lift"]) != 0:
+    if param["pu_tail_lift"] and int(param["pu_tail_lift"]) != 0:
         return {
             "name": "Tail-Gate(pickup)",
             "description": "Applied when there is tail lift pickup. If service is express, then 110% is applied",
-            "value": 52.9396 * 1.1 if 'express' in param['vx_service_name'].lower() else 52.9396,
+            "value": 52.9396 * 1.1
+            if "express" in param["vx_service_name"].lower()
+            else 52.9396,
         }
     else:
         return None
 
 
 def dtl(param):
-    if int(param["de_tail_lift"]) != 0:
+    if param["de_tail_lift"] and int(param["de_tail_lift"]) != 0:
         return {
             "name": "Tail-Gate(delivery)",
             "description": "Applied when there is tail lift delivery. If service is express, then 110% is applied",
-            "value": 52.9396 * 1.1 if 'express' in param['vx_service_name'].lower() else 52.9396,
+            "value": 52.9396 * 1.1
+            if "express" in param["vx_service_name"].lower()
+            else 52.9396,
         }
     else:
         return None
