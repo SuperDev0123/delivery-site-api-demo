430eed77eb421e66a85b6f50e60d136599c97bf6
smartworld-dm <smartworld@email.com>
2021-10-05 04:03:03 +0200

    fix: Plum Hunter BOOK

--- api/fp_apis/payload_builder.py
+++ api/fp_apis/payload_builder.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from api.models import *
 from api.common import common_times as dme_time_lib
 from api.fp_apis.utils import _convert_UOM, gen_consignment_num
-from api.fp_apis.constants import FP_CREDENTIALS, OLD_FP_CREDENTIALS, FP_UOM
+from api.fp_apis.constants import FP_CREDENTIALS, FP_CREDENTIALS, FP_UOM
 from api.helpers.line import is_pallet
 
 logger = logging.getLogger(__name__)  # Payload Builder
@@ -30,9 +30,9 @@ def get_account_detail(booking, fp_name):
         account_code = booking.vx_account_code
 
     if account_code:
-        for client_name in OLD_FP_CREDENTIALS[_fp_name].keys():
-            for key in OLD_FP_CREDENTIALS[_fp_name][client_name].keys():
-                detail = OLD_FP_CREDENTIALS[_fp_name][client_name][key]
+        for client_name in FP_CREDENTIALS[_fp_name].keys():
+            for key in FP_CREDENTIALS[_fp_name][client_name].keys():
+                detail = FP_CREDENTIALS[_fp_name][client_name][key]
 
                 if detail["accountCode"] == account_code:
                     account_detail = detail
