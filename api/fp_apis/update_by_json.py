6d6c754e3dbc2629817f7571022d9fe139d6b2f5
smartworld-dm <smartworld@email.com>
2021-10-07 01:40:40 +0200

    mod: update_by_json

--- api/fp_apis/update_by_json.py
+++ api/fp_apis/update_by_json.py
@@ -1,16 +1,18 @@
 import json
 
+import logging
+
 from django.conf import settings
 
-from api.models import *
-from api.common import sftp
+from api.models import Bookings
+from api.common import sftp, trace_error
 
 if settings.ENV == "local":
     production = False  # Local
 else:
     production = True  # Dev
 
-
+logger = logging.getLogger(__name__)
 # sftp server infos
 sftp_server_infos = {
     "biopak": {
@@ -39,24 +41,32 @@ def build_json(booking):
 
 
 def update_biopak_with_booked_booking(booking_id):
-    booking = Bookings.objects.get(pk=booking_id)
-    json_file_name = (
-        booking.b_clientReference_RA_Numbers + "__" + booking.pk_booking_id + ".json"
-    )
+    LOG_ID = "[BIOPAK UPDATE VIA JSON]"
 
-    if production:
-        local_filepath = "/home/cope_au/dme_sftp/biopak_au/jsons/indata/"
-        local_filepath_archive = "/home/cope_au/dme_sftp/biopak_au/jsons/archive/"
-    else:
-        local_filepath = "./static/jsons/"
-        local_filepath_archive = "./static/jsons/archive/"
+    if not settings.ENV == "prod":
+        return
 
-    json_file = open(local_filepath + json_file_name, "w")
-    json_content = build_json(booking)
-    json_file.write(json_content)
-    json_file.close()
+    try:
+        booking = Bookings.objects.get(pk=booking_id)
+        json_file_name = (
+            booking.b_clientReference_RA_Numbers
+            + "__"
+            + booking.pk_booking_id
+            + ".json"
+        )
 
-    if settings.ENV == "prod":
+        if production:
+            local_filepath = "/home/cope_au/dme_sftp/biopak_au/jsons/indata/"
+            local_filepath_archive = "/home/cope_au/dme_sftp/biopak_au/jsons/archive/"
+        # else:
+        #     local_filepath = "./static/jsons/"
+        #     local_filepath_archive = "./static/jsons/archive/"
+
+        json_file = open(local_filepath + json_file_name, "w")
+        json_content = build_json(booking)
+        json_file.write(json_content)
+        json_file.close()
+
         sftp.upload_sftp(
             sftp_server_infos["biopak"]["host"],
             sftp_server_infos["biopak"]["username"],
@@ -66,3 +76,10 @@ def update_biopak_with_booked_booking(booking_id):
             local_filepath_archive,
             json_file_name,
         )
+        logger.error(
+            f"{LOG_ID} Booking: {booking.b_bookingID_Visual}, Status: SUCCESS!"
+        )
+    except Exception as e:
+        logger.error(f"{LOG_ID} Booking: {booking.b_bookingID_Visual}, Error: {str(e)}")
+        trace_error.print()
+        pass
