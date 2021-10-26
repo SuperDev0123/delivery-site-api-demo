6d7657615673ac546b443355e15e713657829ef2
smartworld-dm <smartworld@email.com>
2021-10-26 08:27:27 +0200

    add: freight_provider to fprouting table

--- api/models.py
+++ api/models.py
@@ -4903,6 +4903,9 @@ class FPRouting(models.Model):
     """
 
     id = models.AutoField(primary_key=True)
+    freight_provider = models.ForeignKey(
+        Fp_freight_providers, on_delete=models.CASCADE, default=None, null=True
+    )
     suburb = models.CharField(max_length=45, default=None, null=True)
     state = models.CharField(max_length=45, default=None, null=True)
     dest_postcode = models.CharField(max_length=45, default=None, null=True)
