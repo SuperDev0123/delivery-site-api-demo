99f0fe53e34373160009ba41bf4bab7b9c6c438b
smartworld-dm <smartworld@email.com>
2021-10-27 01:49:41 +0200

    feat: Plum reprint_label

--- api/clients/plum/index.py
+++ api/clients/plum/index.py
@@ -1391,7 +1391,7 @@ def reprint_label(params, client):
         if not "http" in booking.z_label_url:
             label_url = f"{settings.STATIC_PUBLIC}/pdfs/{booking.z_label_url}"
         else:
-            label_url = f"{settings.STATIC_PUBLIC}/pdfs/{booking.vx_freight_provider.lower}_au/{booking.b_bookingID_Visual}.pdf"
+            label_url = f"{settings.STATIC_PUBLIC}/pdfs/{booking.vx_freight_provider.lower()}_au/{booking.b_bookingID_Visual}.pdf"
 
     # Convert label into ZPL format
     logger.info(f"@369 - converting LABEL({label_url}) into ZPL format...")
