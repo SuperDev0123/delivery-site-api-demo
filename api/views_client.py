a2e00ebaa00de69c216bdf212077fa599e85c0f0
smartworld-dm <smartworld@email.com>
2022-02-23 02:19:15 +0100

    fix: Q issue on views_client

--- api/views_client.py
+++ api/views_client.py
@@ -13,6 +13,7 @@ from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.db import transaction
+from django.db.models import Q
 from rest_framework import views, serializers, status
 from rest_framework.response import Response
 from rest_framework import authentication, permissions, viewsets
