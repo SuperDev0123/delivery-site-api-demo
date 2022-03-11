59fa4ed5fc5596da628a86b1e243e8152fa93035
Daniel <astar1112.rich@outlook.com>
2022-03-11 16:00:22 +0800

    bulk update e_1_Total_dimCubicMeter

---
+++ api/management/commands/total_cubic_meter.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand
+from api.models import (
+    Booking_lines
+)
+from bulk_update.helper import bulk_update
+
+from api.helpers.cubic import get_cubic_meter
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        booking_lines = Booking_lines.objects.filter(
+            e_1_Total_dimCubicMeter__isnull=True,
+            e_dimLength__isnull=False,
+            e_dimWidth__isnull=False,
+            e_dimHeight__isnull=False,
+            e_dimUOM__isnull=False,
+        ).order_by('-pk_lines_id')
+
+        for booking_line in booking_lines:
+            booking_line.e_1_Total_dimCubicMeter = get_cubic_meter(
+                booking_line.e_dimLength,
+                booking_line.e_dimWidth,
+                booking_line.e_dimHeight,
+                booking_line.e_dimUOM,
+            )
+        bulk_update(booking_lines)
+        
