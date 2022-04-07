d278d22649fedde49695e0c5daec7051310a49b5
smartworld-dm <smartworld@email.com>
2022-04-07 05:03:18 +0200

    mod: scans viewset

--- api/views_status.py
+++ api/views_status.py
@@ -20,40 +20,15 @@ from api.serializers import FPStatusHistorySerializer
 logger = logging.getLogger(__name__)
 
 
-class ScansViewSet(viewsets.ViewSet):
+class ScansViewSet(viewsets.ModelViewSet):
     serializer_class = FPStatusHistorySerializer
     permission_classes = [AllowAny]
 
-    @action(detail=False, methods=["get"])
-    def get_scans_from_booking_id(self, request, pk=None):
-        from api.fp_apis.utils import get_dme_status_from_fp_status
+    def get_queryset(self):
+        booking_id = self.request.GET["bookingId"]
+        queryset = FP_status_history.objects.all()
 
-        booking_id = request.GET.get("bookingId")
-        try:
-            if not booking_id:
-                booking = Bookings.objects.all().order_by("-z_CreatedTimestamp")[0]
-                booking_id = booking.id
-            booking = Bookings.objects.get(id=booking_id)
-            fp_status_history = (
-                FP_status_history.objects.values(
-                    "id", "status", "desc", "event_timestamp"
-                )
-                .filter(booking_id=booking_id)
-                .order_by("-event_timestamp")
-            )
-            fp_statuses = [item["status"] for item in fp_status_history]
-            dme_statuses = get_dme_status_from_fp_status(
-                booking.vx_freight_provider, fp_statuses
-            )
-            fp_status_history = [
-                {**item, "desc": dme_statuses[index]}
-                for index, item in enumerate(fp_status_history)
-            ]
-            return Response({"scans": fp_status_history}, status=HTTP_200_OK)
-        except Exception as e:
-            logger.info(f"Get FP status history error: {str(e)}")
-            fp_status_history = []
-            return Response(
-                {"msg": str(e)},
-                status=HTTP_400_BAD_REQUEST,
-            )
+        if booking_id:
+            queryset = queryset.filter(booking_id=booking_id)
+
+        return queryset.order_by("id")
