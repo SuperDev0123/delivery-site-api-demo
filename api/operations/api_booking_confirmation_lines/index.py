32483d54cff1a4b0b30ea5a9dfa37d45569f98fc
smartworld-dm <smartworld@email.com>
2022-04-21 04:25:54 +0200

    fix: api_booking_confirmation_lines creation

--- api/operations/api_booking_confirmation_lines/index.py
+++ api/operations/api_booking_confirmation_lines/index.py
@@ -12,8 +12,7 @@ def create(booking, items):
                 api_item_id=item["item_id"],
             ).save()
     else:
-        for item in items:
-            book_con = Api_booking_confirmation_lines(
-                fk_booking_id=booking.pk_booking_id,
-                label_code=item["label_code"],
-            ).save()
+        book_con = Api_booking_confirmation_lines(
+            fk_booking_id=booking.pk_booking_id,
+            label_code=item["label_code"],
+        ).save()
