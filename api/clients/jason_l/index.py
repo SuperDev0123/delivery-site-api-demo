87123a97bda6ee32299e5f01a5ac2e21023a4e1a
smartworld-dm <smartworld@email.com>
2022-04-21 10:11:32 +0200

    mod: All JasonL bookings to State SA are to book with TNT if DMEA no matter what the price

--- api/clients/jason_l/index.py
+++ api/clients/jason_l/index.py
@@ -21,6 +21,7 @@ from api.models import (
     Pallet,
     API_booking_quotes,
     FP_zones,
+    Api_booking_confirmation_lines,
 )
 from api.serializers import SimpleQuoteSerializer, Simple4ProntoQuoteSerializer
 from api.serializers_client import *
@@ -819,6 +820,13 @@ def push_boks(payload, client, username, method):
                 freight_provider=selected_quote.freight_provider,
                 service_name=selected_quote.service_name,
             )
+        # All JasonL bookings to State SA are to book with TNT if DMEA no matter what the price
+        elif (
+            bok_1.get("shipping_type") == "DMEA"
+            and bok_1.get("b_057_b_del_address_state")
+            and bok_1.get("b_057_b_del_address_state").upper() == "SA"
+        ):
+            quote_set = quote_set.filter(freight_provider="TNT")
         else:
             quote_set = quote_set.exclude(freight_provider__in=["Sendle", "Hunter"])
 
