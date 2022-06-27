10601d896d55bc95f7f237190d4deac11d5fd620
smartworld-dm <smartworld@email.com>
2022-06-27 07:52:18 +0200

    fix: Deliver-ME pricing issue

--- api/fp_apis/built_in/deliver_me.py
+++ api/fp_apis/built_in/deliver_me.py
@@ -10,6 +10,7 @@ def get_pricing(booking, booking_lines):
     inv_cost_quoted, inv_sell_quoted = 0, 0
 
     for line in booking_lines:
+        print("@2 - ", line, inv_cost_quoted, inv_sell_quoted)
         is_pallet = line.e_type_of_packaging.upper() in PALLETS
         length = line.e_dimLength * _get_dim_amount(line.e_dimUOM)
         width = line.e_dimWidth * _get_dim_amount(line.e_dimUOM)
@@ -32,13 +33,14 @@ def get_pricing(booking, booking_lines):
                 if not is_pallet:
                     inv_cost_quoted += 81.45 * cubic_meter * line.e_qty
                     inv_sell_quoted += 137.50 * cubic_meter * line.e_qty
-                elif is_pallet and width <= 1.2 and length <= 1.2:
+                elif is_pallet and length <= 1.2 and width <= 1.2:
                     inv_cost_quoted += 167.86 * line.e_qty
                     inv_sell_quoted += 198.89 * line.e_qty
-                elif is_pallet and width <= 1.2 and length <= 1.6:
+                elif is_pallet and length <= 1.2 and width <= 1.6:
                     inv_cost_quoted += 224.29 * line.e_qty
                     inv_sell_quoted += 268.33 * line.e_qty
-                elif is_pallet and width <= 1.2 and length <= 1.85:
+                    print("@3  ")
+                elif is_pallet and length <= 1.2 and width <= 1.85:
                     inv_cost_quoted += 224.29 * line.e_qty
                     inv_sell_quoted += 268.33 * line.e_qty
             elif (postal_code >= 4000 and postal_code <= 4207) or (
@@ -49,13 +51,13 @@ def get_pricing(booking, booking_lines):
                 if not is_pallet:
                     inv_cost_quoted += 155.03 * cubic_meter * line.e_qty
                     inv_sell_quoted += 212.94 * cubic_meter * line.e_qty
-                elif is_pallet and width <= 1.2 and length <= 1.2:
+                elif is_pallet and length <= 1.2 and width <= 1.2:
                     inv_cost_quoted += 234.25 * line.e_qty
                     inv_sell_quoted += 265.56 * line.e_qty
-                elif is_pallet and width <= 1.2 and length <= 1.6:
+                elif is_pallet and length <= 1.2 and width <= 1.6:
                     inv_cost_quoted += 323.88 * line.e_qty
                     inv_sell_quoted += 471.11 * line.e_qty
-                elif is_pallet and width <= 1.2 and length <= 1.85:
+                elif is_pallet and length <= 1.2 and width <= 1.85:
                     inv_cost_quoted += 323.88 * line.e_qty
                     inv_sell_quoted += 471.11 * line.e_qty
             elif (postal_code >= 5000 and postal_code <= 5199) or (
@@ -66,13 +68,13 @@ def get_pricing(booking, booking_lines):
                 if not is_pallet:
                     inv_cost_quoted += 184.67 * cubic_meter * line.e_qty
                     inv_sell_quoted += 254.28 * cubic_meter * line.e_qty
-                elif is_pallet and width <= 1.2 and length <= 1.2:
+                elif is_pallet and length <= 1.2 and width <= 1.2:
                     inv_cost_quoted += 287.36 * line.e_qty
                     inv_sell_quoted += 321.11 * line.e_qty
-                elif is_pallet and width <= 1.2 and length <= 1.6:
+                elif is_pallet and length <= 1.2 and width <= 1.6:
                     inv_cost_quoted += 403.54 * line.e_qty
                     inv_sell_quoted += 582.22 * line.e_qty
-                elif is_pallet and width <= 1.2 and length <= 1.85:
+                elif is_pallet and length <= 1.2 and width <= 1.85:
                     inv_cost_quoted += 403.54 * line.e_qty
                     inv_sell_quoted += 582.22 * line.e_qty
         # BSD
@@ -85,13 +87,13 @@ def get_pricing(booking, booking_lines):
                 if not is_pallet:
                     inv_cost_quoted += 81.45 * cubic_meter * line.e_qty
                     inv_sell_quoted += 141.5 * cubic_meter * line.e_qty
-                elif is_pallet and width <= 1.2 and length <= 1.2:
+                elif is_pallet and length <= 1.2 and width <= 1.2:
                     inv_cost_quoted += 167.86 * line.e_qty
                     inv_sell_quoted += 206.06 * line.e_qty
-                elif is_pallet and width <= 1.2 and length <= 1.6:
+                elif is_pallet and length <= 1.2 and width <= 1.6:
                     inv_cost_quoted += 224.29 * line.e_qty
                     inv_sell_quoted += 279.08 * line.e_qty
-                elif is_pallet and width <= 1.2 and length <= 1.85:
+                elif is_pallet and length <= 1.2 and width <= 1.85:
                     inv_cost_quoted += 224.29 * line.e_qty
                     inv_sell_quoted += 279.08 * line.e_qty
             elif (postal_code >= 4000 and postal_code <= 4207) or (
@@ -102,13 +104,13 @@ def get_pricing(booking, booking_lines):
                 if not is_pallet:
                     inv_cost_quoted += 155.03 * cubic_meter * line.e_qty
                     inv_sell_quoted += 218.07 * cubic_meter * line.e_qty
-                elif is_pallet and width <= 1.2 and length <= 1.2:
+                elif is_pallet and length <= 1.2 and width <= 1.2:
                     inv_cost_quoted += 234.25 * line.e_qty
                     inv_sell_quoted += 272.44 * line.e_qty
-                elif is_pallet and width <= 1.2 and length <= 1.6:
+                elif is_pallet and length <= 1.2 and width <= 1.6:
                     inv_cost_quoted += 323.88 * line.e_qty
                     inv_sell_quoted += 484.89 * line.e_qty
-                elif is_pallet and width <= 1.2 and length <= 1.85:
+                elif is_pallet and length <= 1.2 and width <= 1.85:
                     inv_cost_quoted += 323.88 * line.e_qty
                     inv_sell_quoted += 484.89 * line.e_qty
             elif (postal_code >= 5000 and postal_code <= 5199) or (
@@ -119,23 +121,21 @@ def get_pricing(booking, booking_lines):
                 if not is_pallet:
                     inv_cost_quoted += 184.67 * cubic_meter * line.e_qty
                     inv_sell_quoted += 257.59 * cubic_meter * line.e_qty
-                elif is_pallet and width <= 1.2 and length <= 1.2:
+                elif is_pallet and length <= 1.2 and width <= 1.2:
                     inv_cost_quoted += 287.36 * line.e_qty
                     inv_sell_quoted += 325.56 * line.e_qty
-                elif is_pallet and width <= 1.2 and length <= 1.6:
+                elif is_pallet and length <= 1.2 and width <= 1.6:
                     inv_cost_quoted += 403.54 * line.e_qty
                     inv_sell_quoted += 591.11 * line.e_qty
-                elif is_pallet and width <= 1.2 and length <= 1.85:
+                elif is_pallet and length <= 1.2 and width <= 1.85:
                     inv_cost_quoted += 403.54 * line.e_qty
                     inv_sell_quoted += 591.11 * line.e_qty
 
-    pu_tail_lift = booking.b_booking_tail_lift_pickup
-    if pu_tail_lift and int(pu_tail_lift) > 1:
+    if booking.pu_no_of_assists and int(booking.pu_no_of_assists) > 1:
         inv_cost_quoted += 30
         inv_sell_quoted += 30
 
-    de_tail_lift = booking.b_booking_tail_lift_deliver
-    if de_tail_lift and int(de_tail_lift) > 1:
+    if booking.de_no_of_assists and int(booking.de_no_of_assists) > 1:
         inv_cost_quoted += 30
         inv_sell_quoted += 30
 
