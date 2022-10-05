818d2186a49fcab81f450119bb11573deb1d165e
Daniel Yang <astar1112.rich@outlook.com>
2022-10-05 09:06:45 +0800

    fix: add comma

--- api/operations/labels/small_label.py
+++ api/operations/labels/small_label.py
@@ -742,7 +742,7 @@ def build_label(
                             "<font size=%s>%s%s, %s, %s, %s, %s, %s</font>"
                             % (
                                 8,
-                                ((booking.puCompany + " ") if (booking.puCompany or "").lower() != (booking.pu_Contact_F_L_Name or "").lower() else ""),
+                                ((booking.puCompany + ", ") if (booking.puCompany or "").lower() != (booking.pu_Contact_F_L_Name or "").lower() else ""),
                                 booking.pu_Contact_F_L_Name or "",
                                 booking.pu_Address_Street_1 or "",
                                 booking.pu_Address_street_2 or "",
@@ -761,7 +761,7 @@ def build_label(
                             "<font size=%s>%s%s, %s, %s, %s, %s</font>"
                             % (
                                 8,
-                                ((booking.puCompany + " ") if (booking.puCompany or "").lower() != (booking.pu_Contact_F_L_Name or "").lower() else ""),
+                                ((booking.puCompany + ", ") if (booking.puCompany or "").lower() != (booking.pu_Contact_F_L_Name or "").lower() else ""),
                                 booking.pu_Contact_F_L_Name or "",
                                 booking.pu_Address_Street_1 or "",
                                 booking.pu_Address_Suburb or "",
